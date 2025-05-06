a4e5aa588a3a3e4a4a7cb37a50f2be2f937ccc05
Cristian Bolivar <cristian@27cobalto.com>
2025-05-06 14:22:45 -0500

    Proyecto base reto

--- src/routes/itemsRoutes.js
+++ src/routes/itemsRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
+
 const router = express.Router();
 const {
   getItems,
   createItem,
   deleteItem,
 } = require('../controllers/itemsController');
+
 router.get('/', getItems);
 router.post('/', createItem);
 router.delete('/:id', deleteItem);
