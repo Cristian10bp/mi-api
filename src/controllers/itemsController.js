2c926bc118ef75ce5c113b20f3386c3ae1d010ca
Cristian Bolivar <cristian@27cobalto.com>
2025-05-06 15:19:19 -0500

    Proyecto base reto

--- src/controllers/itemsController.js
+++ src/controllers/itemsController.js
@@ -1,21 +1,20 @@
 // src/controllers/itemsController.js
 
 const getItems = (req, res) => {
-    res.status(200).json({ message: 'Todos los items' });
-  };
-  
-  const createItem = (req, res) => {
-    res.status(201).json({ message: 'Item creado' });
-  };
-  
-  const deleteItem = (req, res) => {
-    const { id } = req.params;
-    res.status(200).json({ message: `Item con id ${id} eliminado` });
-  };
-  
-  module.exports = {
-    getItems,
-    createItem,
-    deleteItem
-  };
-  
+  res.status(200).json({ message: 'Todos los items' });
+};
+
+const createItem = (req, res) => {
+  res.status(201).json({ message: 'Item creado' });
+};
+
+const deleteItem = (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ message: `Item con id ${id} eliminado` });
+};
+
+module.exports = {
+  getItems,
+  createItem,
+  deleteItem,
+};
