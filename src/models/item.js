37c2d3ead4b1b2d967fd7cb1a408c8f9043039be
Cristian Bolivar <cristian@27cobalto.com>
2025-05-06 14:54:24 -0500

    Proyecto base reto

--- src/models/item.js
+++ src/models/item.js
@@ -1,11 +1,10 @@
 // src/models/item.js
 class Item {
-    constructor(id, title, description) {
-      this.id = id;
-      this.title = title;
-      this.description = description;
-      this.createdAt = new Date();
-    }
-  }  
-  module.exports = Item;
-  
+  constructor(id, title, description) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.createdAt = new Date();
+  }
+}
+module.exports = Item;
